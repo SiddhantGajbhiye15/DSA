d2050155f7254174a4b6139507577198dcacf923
SiddhantGajbhiye15 <15siddhant2004@gmail.com>
2026-07-02 12:32:14 +0530

    resolved Error for invalid array length passed to  function

--- Array/01Easy-Problems/FindMissingNumber.cpp
+++ Array/01Easy-Problems/FindMissingNumber.cpp
@@ -13,9 +13,8 @@ int MissingNumber(int arr[],int n){
 
 int main(){
   
-  int arr[] = {1,2,3,4,5,6,7,8,9};
-
-  int n = sizeof(arr) / sizeof(int);
-
+  int arr[] = {1,2,3,4,5,6,7,8};
+  int n = sizeof(arr)/sizeof(int) + 1;
+  
   cout<<"The missing number is "<<MissingNumber(arr, n)<<endl;
 }
