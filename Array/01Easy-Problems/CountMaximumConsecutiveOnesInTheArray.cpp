0f1091aac64a0e0c493596ab071926374f385a08
SiddhantGajbhiye15 <15siddhant2004@gmail.com>
2026-07-02 12:48:52 +0530

    solved Count Max Consecutive Ones in array problem

---
+++ Array/01Easy-Problems/CountMaximumConsecutiveOnesInTheArray.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int CountMaxConsecutiveOnes(int arr[], int n)
+{
+  int count = 0;
+  int maxCount = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i] == 1)
+    {
+      count++;
+    }
+    else if (arr[i] == 0)
+    {
+      if (count > maxCount)
+      {
+        maxCount = count;
+      }
+      count = 0;
+    }
+  }
+  if (count > maxCount)
+      {
+        maxCount = count;
+      }
+
+  return maxCount;
+}
+
+int main()
+{
+  int arr1[] = {1, 1, 1, 1, 1};
+
+  int n = sizeof(arr1) / sizeof(arr1[0]);
+
+  int maxCount = CountMaxConsecutiveOnes(arr1, n);
+  cout << "Maximum consecutive ones are " << maxCount << endl;
+
+  return 0;
+}
