12cd09be24c60e00bfc006af824f84f4d94c7fb3
SiddhantGajbhiye15 <15siddhant2004@gmail.com>
2026-07-02 12:27:48 +0530

    Solved Find Missing Number in array Problem

---
+++ Array/01Easy-Problems/FindMissingNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int MissingNumber(int arr[],int n){
+  
+  int sum = (n*(n+1))/2;
+  int arrSum = 0;
+  for(int i=0;i<n;i++){
+    arrSum+=arr[i];
+  }
+  return sum-arrSum;
+}
+
+int main(){
+  
+  int arr[] = {1,2,3,4,5,6,7,8,9};
+
+  int n = sizeof(arr) / sizeof(int);
+
+  cout<<"The missing number is "<<MissingNumber(arr, n)<<endl;
+}
