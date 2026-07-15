13d8bca5acf82786605220e9ff1665ebc7d08253
SiddhantGajbhiye15 <15siddhant2004@gmail.com>
2026-07-15 20:57:20 +0530

    solved subbray Sum equals k using prefix sum and map which leads to optimal approach

--- Array/02Medium-Problems/SubarraySumEqualsK.cpp
+++ Array/02Medium-Problems/SubarraySumEqualsK.cpp
@@ -3,19 +3,34 @@ using namespace std;
 
 int subarraySum(vector<int> &nums, int k)
 {
-  int sum = 0, maxCount = 0;
-  for (int i = 0; i < nums.size(); i++)
-  {
-    sum = 0;
-    for (int j = i; j < nums.size(); j++)
-    {
-      sum += nums[j];
-      if (sum == k)
-      {
-        maxCount++;
-      }
+  // arr = {1, 2, 3,-1,2,-1,-1,3,4,-3};
+  int prefixsum = 0, maxCount = 0;
+
+  unordered_map<int,int> map;
+  map[0] = 1;
+  for(int i=0;i<nums.size();i++){
+
+    prefixsum += nums[i];
+    int remove = prefixsum - k;
+
+    if(map.find(remove) != map.end()){
+      maxCount += map[remove];
     }
+    map[prefixsum]++;
   }
+
+  // for (int i = 0; i < nums.size(); i++)
+  // {
+  //   sum = 0;
+  //   for (int j = i; j < nums.size(); j++)
+  //   {
+  //     sum += nums[j];
+  //     if (sum == k)
+  //     {
+  //       maxCount++;
+  //     }
+  //   }
+  // }
   return maxCount;
 }
 
