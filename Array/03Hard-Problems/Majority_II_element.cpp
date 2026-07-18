b38246005b5d61a58e4fbddde735825528476b85
SiddhantGajbhiye15 <15siddhant2004@gmail.com>
2026-07-18 11:17:49 +0530

    feat: solvd majority II problem using optimal approach

--- Array/03Hard-Problems/Majority_II_element.cpp
+++ Array/03Hard-Problems/Majority_II_element.cpp
@@ -8,26 +8,81 @@ class Solution
 public:
   vector<int> majorityElement(vector<int> &nums)
   {
-    int n = nums.size();
-    vector<int> arr;
-    unordered_map<int, int> mp;
+    int count1 = 0, count2 = 0;
+    int candidate1 = 0, candidate2 = 0;
 
-    // Count frequency of each element
-    for (int i = 0; i < n; i++)
+    // First Pass: Find Candidates
+    for (int num : nums)
     {
-      mp[nums[i]]++;
-    }
 
-    // Store elements appearing more than n/3 times
-    for (auto it : mp)
-    {
-      if (it.second > n / 3)
+      if (num == candidate1)
       {
-        arr.push_back(it.first);
+        count1++;
+      }
+      else if (num == candidate2)
+      {
+        count2++;
+      }
+      else if (count1 == 0)
+      {
+        candidate1 = num;
+        count1 = 1;
+      }
+      else if (count2 == 0)
+      {
+        candidate2 = num;
+        count2 = 1;
+      }
+      else
+      {
+        count1--;
+        count2--;
       }
     }
 
-    return arr;
+    // Second Pass: Verify Candidates
+    count1 = 0;
+    count2 = 0;
+
+    for (int num : nums)
+    {
+      if (num == candidate1)
+        count1++;
+      else if (num == candidate2)
+        count2++;
+    }
+
+    vector<int> ans;
+    int n = nums.size();
+
+    if (count1 > n / 3)
+      ans.push_back(candidate1);
+
+    if (count2 > n / 3)
+      ans.push_back(candidate2);
+
+    return ans;
+
+    //   int n = nums.size();
+    //   vector<int> arr;
+    //   unordered_map<int, int> mp;
+
+    //   // Count frequency of each element
+    //   for (int i = 0; i < n; i++)
+    //   {
+    //     mp[nums[i]]++;
+    //   }
+
+    //   // Store elements appearing more than n/3 times
+    //   for (auto it : mp)
+    //   {
+    //     if (it.second > n / 3)
+    //     {
+    //       arr.push_back(it.first);
+    //     }
+    //   }
+
+    //   return arr;
   }
 };
 
